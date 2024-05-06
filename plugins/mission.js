94c2b586100cd55f2fa8e3864ec349b6ba3120df
MohamedWaly8194 <168978219+MohamedWaly8194@users.noreply.github.com>
2024-05-06 07:05:23 +0300

    Update أوامر.js

--- plugins/mission.js
+++ plugins/mission.js
@@ -64,6 +64,7 @@ conn.sendMessage(m.chat, {
 handler.help = ['main']
 handler.tags = ['group']
 handler.command = ['اوامر'] 
+handler.command = ['أوامر'] 
 
 export default handler
 function clockString(ms) {
