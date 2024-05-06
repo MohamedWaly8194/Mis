1dd28049f6bddbec3e1b610f3e922626b870ea74
MohamedWaly8194 <168978219+MohamedWaly8194@users.noreply.github.com>
2024-05-06 07:23:58 +0300

    Update بوت.js

--- plugins/بوت.js
+++ plugins/بوت.js
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-    await conn.sendMessage(m.chat, { react: { text: '🌸', key: m.key } })
+    await conn.sendMessage(m.chat, { react: { text: '✨️', key: m.key } })
 let videoUrl = 'https://telegra.ph/file/3fd883bd07a928da99374.mp4';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
@@ -36,15 +36,15 @@ let more = String.fromCharCode(8206)
 let readMore = more.repeat(900) 
   const taguser = '@' +  m.sender.split('@s.whatsapp.net')[0];
 let str = ` 
-*⧠━──━⧈⇓《🌸》⇓⧈━──━⧠*
-*⧉┇اهلا بك ف بوت مـيـتـســ🌸ـوري┇➥*
+*⧠━──━⧈⇓《✨️》⇓⧈━──━⧠*
+*⧉┇اهلا بك ف بوت روبين┇➥*
 
 *❐↞┇اذا كان امر لا يعمل ابلغ المطور عن طريق امر .بلاغ ┇➥*
 *❂ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ*
 *⏎┇ لعرض الاوامر ↞.اوامر  ➪*
 *⏎┇ لعرض المهام  ↞ .المهام ➪*
 *❂ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ*
-*⧠━──━⧈⇓《🌸》⇓⧈━──━⧠*
+*⧠━──━⧈⇓《✨️》⇓⧈━──━⧠*
 ‬`.trim();
 
 conn.sendMessage(m.chat, {
