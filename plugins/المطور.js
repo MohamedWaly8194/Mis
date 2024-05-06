c4af9961a257e6e4bd7c0871873360c731c20bea
MohamedWaly8194 <168978219+MohamedWaly8194@users.noreply.github.com>
2024-05-06 09:41:36 +0300

    Update المطور.js

--- plugins/المطور.js
+++ plugins/المطور.js
@@ -1,4 +1,4 @@
-import { createHash } from 'crypto'
+٧import { createHash } from 'crypto'
 import { canLevelUp, xpRange } from '../lib/levelling.js'
 import fetch from 'node-fetch'
 import fs from 'fs'
@@ -20,7 +20,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
     await conn.sendMessage(m.chat, { react: { text: '🐲', key: m.key } })
-let videoUrl = 'https://telegra.ph/file/28469c6faa2ae3681443c.mp4';
+let videoUrl = 'https://telegra.ph/file/6b08e4d96d0493dd4945f.mp4';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
